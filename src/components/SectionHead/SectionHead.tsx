23233e35aed9e85a231fa3174f2669f2bb86f144
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-15 15:06:56 -0700

    Update sectionhead

--- src/components/SectionHead/SectionHead.tsx
+++ src/components/SectionHead/SectionHead.tsx
@@ -2,11 +2,9 @@ import styled from "styled-components";
 
 const SectionHead = styled.div`
   margin-top: var(--size-10);
-  margin-bottom: var(--size-4);
-  border-bottom: 1px solid var(--color-ghost);
+  margin-bottom: var(--size-sm);
   color: var(--color-navy);
   font-size: var(--size-4);
-  padding-bottom: var(--size-sm);
 `;
 
 export default SectionHead;
