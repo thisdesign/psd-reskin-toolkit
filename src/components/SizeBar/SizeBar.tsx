3be061817122a3023f95793c6342d9c0ce41b984
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-16 11:54:05 -0700

    Add px value to top bar

--- src/components/SizeBar/SizeBar.tsx
+++ src/components/SizeBar/SizeBar.tsx
@@ -35,7 +35,9 @@ const SizeBar = () => {
   return (
     <S.Outline>
       <div>Device Width: {size}px</div>
-      <div>Breakpoint: {breakpoint.toUpperCase()}</div>
+      <div>
+        Breakpoint: {breakpoint.toUpperCase()} (&lt;{SIZES[breakpoint]}px)
+      </div>
       <div>Base Font Size: {FONT_SIZES[breakpoint]}px</div>
     </S.Outline>
   );
