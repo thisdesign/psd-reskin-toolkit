79950b923e76471057ca3f28fb34c4e3c3be5f78
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-16 10:40:32 -0700

    Add title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Link from "next/link";
 import { Wrapper, SizeBar, H4, Paragraph, Section } from "components";
 import { withRouter, Router } from "next/router";
+import Head from "next/head";
 import GlobalStyle from "../../style/GlobalStyle";
 import S from "./Layout.Styled";
 
 const Layout: React.FC<{ router: Router }> = ({ children, router }) => {
   return (
     <div>
+      <Head>
+        <title>PS&amp;D Style Guide</title>
+      </Head>
       <GlobalStyle />
       <SizeBar />
       <S.Header>
