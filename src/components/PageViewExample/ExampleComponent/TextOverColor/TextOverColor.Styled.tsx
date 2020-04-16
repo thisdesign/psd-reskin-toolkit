2d9f65a740ff5be9f16de884b2e449fd9c49096b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-16 16:45:56 -0700

    Brighten text on navy

--- src/components/PageViewExample/ExampleComponent/TextOverColor/TextOverColor.Styled.tsx
+++ src/components/PageViewExample/ExampleComponent/TextOverColor/TextOverColor.Styled.tsx
@@ -4,6 +4,10 @@ const TextOverColor = styled.div`
   background: var(--color-navy);
   color: white;
   padding: var(--size-14) 0;
+
+  p {
+    color: var(--color-heather);
+  }
 `;
 
 export default {
