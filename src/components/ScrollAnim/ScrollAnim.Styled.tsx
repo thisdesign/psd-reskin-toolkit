c66b95578999b0b7a2de554c57ec2e7004c24712
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-23 16:42:07 -0700

    vh based translate

--- src/components/ScrollAnim/ScrollAnim.Styled.tsx
+++ src/components/ScrollAnim/ScrollAnim.Styled.tsx
@@ -3,7 +3,7 @@ import { FADE_IN_TIMING } from "../../constants";
 
 const Wrapper = styled.div<{ inView: boolean }>`
   opacity: 0;
-  transform: translate3d(0, var(--size-3), 0);
+  transform: translate3d(0, 1.5vh, 0);
 
   transition: ${FADE_IN_TIMING}ms opacity cubic-bezier(var(--ease-decel)),
     ${FADE_IN_TIMING}ms transform cubic-bezier(var(--ease-decel));
