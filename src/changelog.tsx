dfddc0ece6cea9a100000a327e61d31ee290c7cb
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-29 13:35:08 -0700

    type changes

--- src/changelog.tsx
+++ src/changelog.tsx
@@ -1,16 +1,27 @@
 type Version = {
   number: string;
   changes: string[];
+  date: Date;
 };
 
 export const VERSIONS: Version[] = [
   {
     number: "0.1.0",
     changes: ["Initial version"],
+    date: new Date("4/9/20"),
   },
   {
     number: "0.1.1",
     changes: ["Added Midnight Color"],
+    date: new Date("4/29/20"),
+  },
+  {
+    number: "0.1.2",
+    changes: [
+      "Reduced h2 and h1 heading sizes at tablet breakpoint",
+      "Added /sketch/ route for This design team.",
+    ],
+    date: new Date("4/29/20"),
   },
 ];
 
--- src/components/PageViewSketch/PageViewSketch.tsx
+++ src/components/PageViewSketch/PageViewSketch.tsx
@@ -2,63 +2,32 @@ import React from "react";
 import { Layout, Wrapper, Section, H1, InlineCode } from "components";
 import Link from "next/link";
 import S from "./PageViewSketch.Styled";
-import { SCALE, SIZES, FONT_SIZES } from "../../constants";
+import { FONT_SIZE_SCALE, SIZES, SCALE, FONT_SIZES } from "../../constants";
 
 const keys = [...Object.keys(FONT_SIZES)].reverse();
 
 type Size = {
   size: number;
-  base: number;
+
   name: string;
-  headings: {
-    h1: number;
-    h2: number;
-    h3: number;
-    h4: number;
-    h5: number;
-    h6: number;
-  };
+  sizeCode: string;
 };
 
 const TYPE_SIZES: Size[] = [
   {
     size: 375,
-    base: FONT_SIZES.xxs,
     name: "Mobile",
-    headings: {
-      h1: 6,
-      h2: 5,
-      h3: 4,
-      h4: 3,
-      h5: 2,
-      h6: 1,
-    },
+    sizeCode: "xxs",
   },
   {
     size: 768,
-    base: FONT_SIZES.sm,
     name: "Tablet",
-    headings: {
-      h1: 8,
-      h2: 6,
-      h3: 4,
-      h4: 3,
-      h5: 2,
-      h6: 1,
-    },
+    sizeCode: "sm",
   },
   {
     size: 1280,
-    base: FONT_SIZES.md,
     name: "Desktop",
-    headings: {
-      h1: 8,
-      h2: 6,
-      h3: 4,
-      h4: 3,
-      h5: 2,
-      h6: 1,
-    },
+    sizeCode: "md",
   },
 ];
 
@@ -76,25 +45,35 @@ const PageViewTypeSize: React.FC<{ browserSize?: string }> = ({
           <H1>Sketch Guidelines</H1>
         </Section>
         <S.TableWrapper>
-          {TYPE_SIZES.map((item) => (
-            <S.TableCol>
-              <S.TableCell head>{item.name}</S.TableCell>
+          {TYPE_SIZES.map(({ name, size, sizeCode }) => (
+            <S.TableCol key={name}>
+              <S.TableCell head>{name}</S.TableCell>
               <S.TableCell>
-                Artboard: <InlineCode> {item.size}px</InlineCode>
+                Artboard: <InlineCode> {size}px</InlineCode>
               </S.TableCell>
               <S.TableCell>
-                Base font: <InlineCode>{item.base}px</InlineCode>
+                Base font: <InlineCode>{FONT_SIZES[sizeCode]}px</InlineCode>
               </S.TableCell>
-              {Object.keys(item.headings).map((key) => {
-                const scaleIndex = item.headings[key];
-                const scaleFactor = SCALE[scaleIndex];
-                const fontSize = scaleFactor * item.base;
+              {Object.keys(FONT_SIZE_SCALE).map((hSize) => {
+                const sizeKeys = Object.keys(SIZES);
+                const bkptCandidates = [...sizeKeys].slice(
+                  sizeKeys.indexOf(sizeCode),
+                  sizeKeys.length
+                );
+
+                const headingSizeCode = bkptCandidates.reduce((acc, cur) => {
+                  const foundSize = FONT_SIZE_SCALE[hSize][cur];
+
+                  return foundSize && acc === -1 ? foundSize : acc;
+                }, -1);
+
+                const scaleFactor = SCALE[headingSizeCode];
+                const fontSize = scaleFactor * FONT_SIZES[sizeCode];
                 const fontSizeRounded = Math.round(fontSize);
 
                 return (
-                  <S.TableCell>
-                    {key.toUpperCase()}:{" "}
-                    <InlineCode>{fontSizeRounded}px</InlineCode>
+                  <S.TableCell key={hSize}>
+                    {hSize}: <InlineCode>{fontSizeRounded}px</InlineCode>
                   </S.TableCell>
                 );
               })}
